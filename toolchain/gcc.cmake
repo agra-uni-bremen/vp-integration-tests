bec09fa0de3deeb52072acfec92f6bf3277ffdd9
Sören Tempel <tempel@uni-bremen.de>
2020-01-13 12:59:50 +0100

    toolchain: fix gcc configuration
    * Set compiler to riscv64-unknown-elf-gcc as shipped by [1]
    * Set CMAKE_SYSTEM_NAME to Generic to prevent compiler tests
      with -rdynamic flag.
    
    [1]: https://github.com/riscv/riscv-gnu-toolchain

--- toolchain/gcc.cmake
+++ toolchain/gcc.cmake
@@ -1,11 +1,11 @@
 # CMake cross toolchain for riscv with gcc
 # See: https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-linux
 
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR riscv)
 
-set(CMAKE_C_COMPILER riscv64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER riscv64-linux-gnu-gcc)
+set(CMAKE_C_COMPILER riscv64-unknown-elf-gcc)
+set(CMAKE_CXX_COMPILER riscv64-unknown-elf-gcc)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
