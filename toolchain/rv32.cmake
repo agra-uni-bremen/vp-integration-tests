b31664afc82982946ff14fd9aeb8b33ef86a2f2f
Sören Tempel <tempel@uni-bremen.de>
2020-01-16 18:46:52 +0100

    toolchain: add GCC cross toolchain for RV32

---
+++ toolchain/rv32.cmake
@@ -0,0 +1,18 @@
+# CMake cross toolchain for RV32 with GCC
+# See: https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-linux
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR riscv)
+
+set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
+set(CMAKE_CXX_COMPILER riscv32-unknown-elf-g++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE NEVER)
+
+# Set Architecture and ABI flags
+set(CMAKE_C_FLAGS "-march=rv32g -mabi=ilp32d")
+set(CMAKE_CXX_FLAGS "-march=rv32g -mabi=ilp32d")
+set(CMAKE_ASM_FLAGS "-march=rv32g -mabi=ilp32d")
